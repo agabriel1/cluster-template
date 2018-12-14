56caf9e48b1e154ab91d664d5d7bb390b621cc44
agabriel1 <43051089+agabriel1@users.noreply.github.com>
2018-12-13 20:58:37 -0500

    Update client.sh

--- client.sh
+++ client.sh
@@ -23,5 +23,5 @@ sudo mount -t nfs 192.168.1.3:/scratch /scratch
 echo "192.168.1.1:/software /software  nfs defaults 0 0" >> /etc/fstab
 echo "192.168.1.3:/scratch /scratch  nfs defaults 0 0" >> /etc/fstab
 
-echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/al844976/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/al844976/.bashrc
+echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/ag899460/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/ag899460/.bashrc
